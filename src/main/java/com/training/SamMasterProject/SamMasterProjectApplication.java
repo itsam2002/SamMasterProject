488d0cbadd7a2ba25ea529b11c8e4465edbf4592
itsam2002 <51936666+itsam2002@users.noreply.github.com>
2025-12-12 12:55:27 -0500

    RequestParam and PathVariable Concept

--- src/main/java/com/training/SamMasterProject/SamMasterProjectApplication.java
+++ src/main/java/com/training/SamMasterProject/SamMasterProjectApplication.java
@@ -6,15 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SamMasterProjectApplication implements CommandLineRunner {
+public class SamMasterProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SamMasterProjectApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Calc calc = new Calc();
-		System.out.println(calc.add(5,10));
-	}
 }
