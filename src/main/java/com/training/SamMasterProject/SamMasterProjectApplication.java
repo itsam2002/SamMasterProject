ca62b5cb5fb1264549cdf906d7a37385cafd5dd8
itsam2002 <itsam2002@gmail.com>
2025-12-12 09:10:53 -0500

    Initial commit

---
+++ src/main/java/com/training/SamMasterProject/SamMasterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.training.SamMasterProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamMasterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamMasterProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/SamMasterProject/SamMasterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.SamMasterProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamMasterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
