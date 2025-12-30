ce627ee92c9eff331d926890e9110ee3b0f26373
itsam2002 <51936666+itsam2002@users.noreply.github.com>
2025-12-30 18:26:28 -0500

    Springboot Hibernate Concept

--- src/main/java/com/training/SamMasterProject/Controller/MyController.java
+++ src/main/java/com/training/SamMasterProject/Controller/MyController.java
@@ -3,9 +3,12 @@ package com.training.SamMasterProject.Controller;
 import com.training.SamMasterProject.Entity.Employee;
 import com.training.SamMasterProject.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MyController {
@@ -16,5 +19,10 @@ public class MyController {
     @PostMapping("/employee")
     public Employee saveEmployee(@RequestBody Employee employee){
         return employeeService.insertEmployee(employee);
+    }
+
+    @GetMapping("/employee")
+    public List<Employee> listOfEmp(){
+        return employeeService.getEmployees();
     }
 }
--- src/main/java/com/training/SamMasterProject/Service/EmployeeService.java
+++ src/main/java/com/training/SamMasterProject/Service/EmployeeService.java
@@ -5,6 +5,9 @@ import com.training.SamMasterProject.Repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class EmployeeService {
 
@@ -15,4 +18,8 @@ public class EmployeeService {
         return employeeRepository.save(employee);
     }
 
+    public List<Employee> getEmployees(){
+        List<Employee> employeeList = employeeRepository.findAll();
+        return employeeList;
+    }
 }
